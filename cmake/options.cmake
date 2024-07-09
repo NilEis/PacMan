c10bb56815aff3313c1abaef0faeff321cb3b446
NilEis <52820950+NilEis@users.noreply.github.com>
2024-07-09 23:20:17 +0200

    fix: action

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,11 +1,14 @@
 set(SDL_DIALOG
-    OFF
-    CACHE BOOL "" FORCE)
+        OFF
+        CACHE BOOL "" FORCE)
 
 set(SDL_STATIC
         ON
         CACHE BOOL "" FORCE)
 
 set(SDL_TEST_LIBRARY
+        OFF
+        CACHE BOOL "" FORCE)
+set(SDL3IMAGE_WERROR
         OFF
         CACHE BOOL "" FORCE)
